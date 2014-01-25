aa9024739a8c0fc7c5d2adf0739eed60b5d3762d
Horacio G. de Oro <hgdeoro@gmail.com>
2014-01-25 12:57:20 -0300

    Update

--- garnish.py
+++ garnish.py
@@ -18,7 +18,10 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
 
-from StringIO import StringIO
+#
+# TODO: add "copyright" exif tags
+#
+
 import datetime
 import logging
 import os
